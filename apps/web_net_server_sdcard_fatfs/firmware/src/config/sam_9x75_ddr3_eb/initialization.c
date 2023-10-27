b3ee12ce96f4bd9bbd27fe929422db3f0cafad32
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-10-27 09:59:17 +0530

    [MH3-84551] web_net_server_sdcard_fatfs application regeneration - sam_9x75_ddr3_eb

--- apps/web_net_server_sdcard_fatfs/firmware/src/config/sam_9x75_ddr3_eb/initialization.c
+++ apps/web_net_server_sdcard_fatfs/firmware/src/config/sam_9x75_ddr3_eb/initialization.c
@@ -320,6 +320,7 @@ const TCPIP_STACK_MODULE_CONFIG TCPIP_STACK_MODULE_CONFIG_TBL [] =
     {TCPIP_MODULE_SNTP,             &tcpipSNTPInitData},            // TCPIP_MODULE_SNTP
 
     {TCPIP_MODULE_HTTP_NET_SERVER,  &tcpipHTTPNetInitData},         // TCPIP_MODULE_HTTP_NET_SERVER
+    {TCPIP_MODULE_COMMAND,          0},                             // TCPIP_MODULE_COMMAND,
     { TCPIP_MODULE_MANAGER,         &tcpipHeapConfig },             // TCPIP_MODULE_MANAGER
 
 // MAC modules
