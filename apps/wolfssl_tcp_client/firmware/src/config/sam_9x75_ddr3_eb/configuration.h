20dd9d8b21a98f04754c93cfe1128221c2049620
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-10-30 09:42:04 +0530

    [MH3-84559] wolfssl_tcp_client application regeneration- sam_9x75_ddr3_eb

--- apps/wolfssl_tcp_client/firmware/src/config/sam_9x75_ddr3_eb/configuration.h
+++ apps/wolfssl_tcp_client/firmware/src/config/sam_9x75_ddr3_eb/configuration.h
@@ -110,7 +110,7 @@ extern "C" {
 #define SYS_CONSOLE_DEVICE_MAX_INSTANCES   			(1U)
 #define SYS_CONSOLE_UART_MAX_INSTANCES 	   			(1U)
 #define SYS_CONSOLE_USB_CDC_MAX_INSTANCES 	   		(0U)
-#define SYS_CONSOLE_PRINT_BUFFER_SIZE        		(200U)
+#define SYS_CONSOLE_PRINT_BUFFER_SIZE        		(4096U)
 
 
 
@@ -296,7 +296,7 @@ extern "C" {
 
 /*** TCPIP Heap Configuration ***/
 #define TCPIP_STACK_USE_INTERNAL_HEAP
-#define TCPIP_STACK_DRAM_SIZE                       81920
+#define TCPIP_STACK_DRAM_SIZE                       153600
 #define TCPIP_STACK_DRAM_RUN_LIMIT                  2048
 
 #define TCPIP_STACK_MALLOC_FUNC                     malloc
