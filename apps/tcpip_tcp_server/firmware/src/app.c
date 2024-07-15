735c99b5f52b5fe28af7b3b2679677c1655ee395
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-07-15 11:50:03 +0530

    [MH3-97113] - Added tcpip_tcp_server application - sam_9x75_curiosity

--- apps/tcpip_tcp_server/firmware/src/app.c
+++ apps/tcpip_tcp_server/firmware/src/app.c
@@ -268,6 +268,7 @@ void APP_Tasks ( void )
     }
 }
 
+#ifndef sam_9x75_curiosity
 void AppKSZ9131ResetFunction(const struct DRV_ETHPHY_OBJECT_BASE_TYPE* pBaseObj, DRV_HANDLE handle)
 {
 
@@ -285,6 +286,7 @@ void AppKSZ9131ResetFunction(const struct DRV_ETHPHY_OBJECT_BASE_TYPE* pBaseObj,
     }
 
 }
+#endif
 /*******************************************************************************
  End of File
  */
