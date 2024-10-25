29a5d0591e5929af8b92c590a6384eb99cfb6464
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-10-25 09:02:53 +0530

    [MH3-104544] - Documentation updates

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {24:15,12:2,11:2,3:-1,23:2,19:2,28:3,13:2,26:2,6:2,18:15,21:2,1:3,17:2,27:2,16:2,7:3,10:2,2:-1,25:3,15:-1,31:15,8:15,5:2,29:2,22:2,30:3,9:2,0:3,20:2,14:3,4:3};
-});
